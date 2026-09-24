3ba66194072f8e2edd860acf81a06a4ed69a2493
Louis <louis@degand.cloud>
2026-09-24 17:38:26 +0200

    Fixed some JS bugs, still ressembles nothing tho

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-function init(){
+function init() {
     console.log('Script Successfully loaded.')
     console.log('Window width : ' + window.innerWidth)
     console.log('Window height : ' + window.innerHeight)
 }
 
 
-// Constantes de taille de fenetre
-const HEIGHT = window.innerHeight
-const WIDTH = window.innerWidth
+// Window size variables
+let HEIGHT = window.innerHeight
+let WIDTH = window.innerWidth
 
-// Constante de jeu
-const game = document.getElementById('gameCanvas')
+// Game constants
+const game = document.getElementById("gameCanvas");
 
 
-// Fonction pour resize la fenêtre
-function resize(){
-    game.style.innerHeight=HEIGHT
-    game.style.width=WIDTH
+// Window size info function
+function getWindowSize() {
+    HEIGHT = window.innerHeight
+    WIDTH = window.innerWidth
+    console.log('New windows info : ' + HEIGHT + " / " + WIDTH)
+}
+
+// Window resizing function
+function resize() {
+    game.style.height = HEIGHT
+    game.style.width = WIDTH
 }
