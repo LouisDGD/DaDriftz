4bdeaa45c9f9aac57a114b26f30eeb5111db87b6
Louis <louis@degand.cloud>
2026-09-24 18:40:35 +0200

    IT FUCKING MOVES !!

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const ctx = game.getContext("2d")
 // Global variables
 let HEIGHT
 let WIDTH
+let posX = 10
+let posY = 10
+let movementX = 0
+let movementY = 0
 
 
 function init() {
@@ -13,17 +17,19 @@ function init() {
     console.log('Window height : ' + window.innerHeight)
 
     window.addEventListener('resize', resize)
+    window.addEventListener('keydown', keyTriggered)
 
     resize()
-    initCanvas()
+    gameLoop()
 }
 
 
-// Initiating the canvas
-function initCanvas() {
-    ctx.fillStyle = "blue"
-    ctx.fillRect(10, 10, 100, 100);
-}
+// Initiating the canvas - old artifact
+//function initCanvas() {
+//    ctx.fillStyle = "blue"
+//    ctx.fillRect(25, 25, 100, 100);
+//    gameLoop()
+//}
 
 
 // Window size info function
@@ -40,3 +46,33 @@ function resize() {
     game.height = HEIGHT
     game.width = WIDTH
 }
+
+
+// GAMELOOP Function, this one is kinda important
+function gameLoop() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT)
+    ctx.fillStyle = "blue"
+    ctx.fillRect(posX, posY, 25, 25);
+    move()
+    requestAnimationFrame(gameLoop)
+}
+
+// Vehicle moving function
+function move() {
+    posX = posX + movementX
+    posY = posY + movementY
+}
+
+
+// Vehicle key related function
+function keyTriggered(key){
+    console.log("Key triggered : " + key.key)
+         if (key.key == 'z'){movementX = 0, movementY = -1}
+    else if (key.key == 's'){movementX = 0, movementY = 1}
+    else if (key.key == 'q'){movementX = -1, movementY = 0}
+    else if (key.key == 'd'){movementX = 1, movementY = 0}
+    else if (key.key == ' '){movementX = 0, movementY = 0}
+
+}
+
+
